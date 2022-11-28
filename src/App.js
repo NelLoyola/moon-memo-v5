8f008635e344cd3b0e8d208c1a978864790355e5
Danielle Loyola <danielle.loyola@developersinstitute.co.nz>
2022-11-29 12:35:22 +1300

    added a react component that pulls date, time and moonphase fron an external api and renders on client

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
+import React from "react";
+import axios from "axios";
 import logo from "./Moon Memo.png";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
-}
+export default class PhaseList extends React.Component {
+  state = {
+    phases: [],
+  };
 
-export default App;
+  componentDidMount() {
+    axios
+      .get(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/New Zealand?unitGroup=us&key=5HLAMT6C2LQHXCZDZMH7X69WA&include=days&elements=datetime,moonphase`
+      )
+      .then((res) => {
+        const phases = res.data.days;
+        this.setState({ phases });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <ul>
+          {this.state.phases.map((phase) => (
+            <p key={phase.datetime}>{phase.moonphase}</p>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
